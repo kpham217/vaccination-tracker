e8a20e747b647f9b1e151ef78c5a440d6f15a08a
truong.nhu <35893692+thanhtruongnhu@users.noreply.github.com>
2021-06-08 18:22:04 -0300

    Add Scheduler, Authentication for bot, Multi-threading (1.site selection algo, 2.Twitter bot)

---
+++ twitter_bot.py
@@ -0,0 +1,37 @@
+import tweet_bot_credential
+import schedule
+import time
+
+
+def initialize_api():
+    api = tweet_bot_credential.create_api()
+    return api
+
+
+def create_tweet(api, site_list):
+    site_list = site_list[0:2]
+    content = f"Earliest vaccination dates\n\n"
+    for item in site_list:
+        new = f"{item['siteName']}\n{item['readableBookingTime']}\n\n"
+        content = content + new
+
+    print(content)
+    api.update_status(content)
+    print('> Content posted on Twitter!')
+
+
+def set_order(region_count, counter):
+    counter += 1
+    if counter == region_count:
+        counter = 0
+    return counter
+
+
+def create_bot(array, region_count):
+    time.sleep(120)
+    counter = 0
+    api = initialize_api()
+    schedule.every(2).minutes.do(create_tweet, api=api, site_list=array[set_order(region_count, counter)])
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
